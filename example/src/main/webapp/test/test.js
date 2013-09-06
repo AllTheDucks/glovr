19c6ce4f30da97f12fb6b38abf2bb85ba4b27b8a
Wiley Fuller <wiley.fuller@gmail.com>
2013-09-06 23:41:18 +1000

    restructured and added plovrBuild task

---
+++ example/src/main/webapp/test/test.js
@@ -0,0 +1,7 @@
+/**
+ * Created with IntelliJ IDEA.
+ * User: wiley
+ * Date: 6/09/13
+ * Time: 9:11 PM
+ * To change this template use File | Settings | File Templates.
+ */
--- glovr/src/main/groovy/com/wileyfuller/GlovrPlugin.groovy
+++ glovr/src/main/groovy/com/wileyfuller/GlovrPlugin.groovy
@@ -1,5 +1,6 @@
 package com.wileyfuller
 
+import com.google.gson.Gson
 import org.gradle.api.Project
 import org.gradle.api.Plugin
 import org.gradle.api.tasks.JavaExec
@@ -11,11 +12,12 @@ class GlovrPlugin implements Plugin<Project> {
     File plovrJar = new File("$plovrHome/$plovrJarName")
     File tmpDir = new File(System.getProperty('java.io.tmpdir'))
 
+
     void apply(Project project) {
 //        project.extensions.create("greeting", GreetingPluginExtension)
         project.task('plovrServe') << {
             checkPlovrJar(project)
-            String configFileName = generatePlovrConfig(project)
+            String configFileName = generatePlovrServeConfig(project)
             project.javaexec {
                 main = 'org.plovr.cli.Main'
                 classpath = project.files(plovrJar)
@@ -26,9 +28,18 @@ class GlovrPlugin implements Plugin<Project> {
 
 
         project.task('plovrBuild') << {
+            checkPlovrJar(project)
+            String configFileName = generatePlovrBuildConfig(project)
+            project.javaexec {
+                main = 'org.plovr.cli.Main'
+                classpath = project.files(plovrJar)
+                args 'build', tmpDir.absolutePath + '/' + configFileName
+                systemProperty 'java.net.preferIPv4Stack', 'true'
+            }
 
         }
 
+        project.war.dependsOn project.plovrBuild
 
     }
 
@@ -40,18 +51,36 @@ class GlovrPlugin implements Plugin<Project> {
                     dest: plovrHome
             )
         } else {
-            println "Don't need plovr jar"
+            //TODO Change this to an INFO level log statement
+            //println "Plovr jar already downloaded"
 
         }
     }
 
-    String generatePlovrConfig(Project project) {
+    String generatePlovrServeConfig(Project project) {
         String fileName = project.name + "-plovr-SERVE.js"
         File configFile = new File(tmpDir, fileName)
         configFile.withWriter { out ->
-            out.write("{\"id\": \"sample\", \"paths\": \".\"," +
+            out.write("{\"id\": \"$project.name\", \"paths\": \".\"," +
                     "  \"inputs\": \"main.js\"}")
         }
+        return fileName;
+    }
+
+    String generatePlovrBuildConfig(Project project) {
+        String fileName = project.name + "-plovr-BUILD.js"
+        File configFile = new File(tmpDir, fileName)
+        def configMap = [id: project.name, paths: new String("$project.projectDir/src/main/javascript"),
+                inputs: new String("$project.projectDir/src/main/javascript/main.js"),
+                'output-file': new String("$project.buildDir/plovr/compiled/main.js")]
+        Gson gson = new Gson();
+        configFile.withWriter { out ->
+            out.write(gson.toJson(configMap))
+//            out.write("{\"id\": \"$project.name\", \"paths\": \"$project.projectDir/src/main/javascript\"," +
+//                    "  \"inputs\": \"$project.projectDir/src/main/javascript/main.js\"," +
+//                    "\"output-file\":\"$project.buildDir/plovr/compiled/main.js\"}")
+        }
+        project.war.from "$project.buildDir/plovr/compiled/main.js"
         return fileName;
     }
 }
